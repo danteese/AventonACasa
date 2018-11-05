34d8cc07d4098bb41a1613799c903efcd0e32096
Danteese <dalnte@me.com>
2018-11-04 22:19:09 -0600

    Navigation enabled

---
+++ Aventon/ViewControllers/PublicTripsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PublicTripsViewController.swift
+//  Aventon
+//
+//  Created by Dante Bazaldua on 11/4/18.
+//  Copyright © 2018 Dante Bazaldua. All rights reserved.
+//
+
+import UIKit
+
+class PublicTripsViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
